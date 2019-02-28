065742ea9ad54aac242b3d5c39bc3ce21becb300
dragon28rus <_cfytr_@mail.ru>
2019-02-28 18:55:31 +0900

    Update start.sh
    добавленны действия при определдении типа коммутатора

--- start.sh
+++ start.sh
@@ -9,60 +9,60 @@ while [ $nas_start -ne $nas_stop ]
 	do
 		if ping 10.99.$nas_start.$ip -c 4 | grep '100% packet loss'
 			then
-				echo "swich 10.99.$nas_start.$ip not connected" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet/SNTP/scripts/log/ping/log.log
+				echo "swich 10.99.$nas_start.$ip not connected" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/ping/log.log
 			else
-				echo "swich 10.99.$nas_start.$ip is connected" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet/SNTP/scripts/log/ping/log.log
+				echo "swich 10.99.$nas_start.$ip is connected" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/ping/log.log
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'DES-3200'
 					then
-					.... # действие
+					echo "swich 10.99.$nas_start.$ip is DES-3200" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'DES-3528'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is DES-3528" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2995G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2995G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2990G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2990G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2985G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2985G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2982G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2982G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2980G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2980G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2965'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2965" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2960-48G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2960-48G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2960-24G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2960-24G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2950-24G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2950-24G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2940-8G-v2'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2940-8G-v2" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				if snmpwalk -v2c -c protected 10.99.$nas_start.$ip .1.3.6.1.2.1.1.1.0 | grep 'SNR-S2940-8G'
 					then
-					.... #действие
+					echo "swich 10.99.$nas_start.$ip is SNR-S2940-8G" >> /mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet-via-bash/log/hardware.log # действие
 				fi
 				
 				#/mnt/c/Users/dragon28rus/YandexDisk/Документы/Проекты/telnet/SNTP/scripts/snmp_01.sh $ip $nas 
